2db31d04a8af094aec4610b4c7f0067061f1e73c
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2026-04-18 14:06:49 +0530

    default theme correction

--- src/js/script.js
+++ src/js/script.js
@@ -14,14 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // THEME SWITCHER LOGIC
     const themeToggle = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'light';
+    const currentTheme = localStorage.getItem('theme') || 'dark';
 
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     if (themeToggle) {
         themeToggle.addEventListener('click', () => {
             const theme = document.documentElement.getAttribute('data-theme');
-            const newTheme = theme === 'light' ? 'dark' : 'light';
+            const newTheme = theme === 'dark' ? 'light' : 'dark';
 
             document.documentElement.setAttribute('data-theme', newTheme);
             localStorage.setItem('theme', newTheme);
